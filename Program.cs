37f064c35e62235730a1518ad0d17796ff40b33b
clinhares <cleiton@hiper.com.br>
2018-10-22 19:38:10 -0300

    teste push 2

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("branch local cleiton");
+            Console.WriteLine("branch local cleiton 2");
         }
     }
 }
